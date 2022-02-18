035a8b391215d2fb902c6d93ede9dfc62ffadb32
Thiago Pereira <thiagopereiramail@gmail.com>
2022-02-18 17:13:11 +0000

    finaliza os testes

--- __tests__/leftRotation.test.js
+++ __tests__/leftRotation.test.js
@@ -1,4 +1,5 @@
 /*
+
 Uma operação de rotação à esquerda em uma matriz desloca cada um dos elementos da matrizunidade 
 à esquerda. Por exemplo, se 2 rotações à esquerda são realizadas na matriz [1,2,3,4,5], então a 
 matriz se tornaria [3,4,5,1,2].
@@ -15,7 +16,65 @@ separados por espaço.
 import { leftRotation } from "../src/leftRotation";
 
 describe("Left Rotation", () => {
+  test("When A value is NULL should be return 'A value cannot be NULL'", () => {
+    expect(() => leftRotation(null, 10)).toThrow("A value cannot be NULL");
+  });
+
+  test("When A value is UNDEFINED should be return 'A value cannot be []'", () => {
+    expect(() => leftRotation(undefined, 10)).toThrow("A value cannot be []");
+  });
+
+  test("When A value is [] should be return 'A value cannot be []'", () => {
+    expect(() => leftRotation([], 10)).toThrow("A value cannot be []");
+  });
+
+  test("When D value is NULL should be return 'D value cannot be NULL'", () => {
+    expect(() => leftRotation([1, 2, 3, 4, 5], null)).toThrow(
+      "D value cannot be NULL"
+    );
+  });
+
+  test("When D value is UNDEFINED should be return 'D value cannot be NULL'", () => {
+    expect(() => leftRotation([1, 2, 3, 4, 5], undefined)).toThrow(
+      "D value cannot be NULL"
+    );
+  });
+
+  test("When D value is 0 should be return 'D value cannot be NULL'", () => {
+    expect(() => leftRotation([1, 2, 3, 4, 5], 0)).toThrow(
+      "D value cannot be NULL"
+    );
+  });
+
+  test("When A is [1, 2, 3, 4, 5] and D is 1 should be return '2 3 4 5 1'", () => {
+    expect(leftRotation([1, 2, 3, 4, 5], 1)).toEqual("2 3 4 5 1");
+  });
+
   test("When A is [1, 2, 3, 4, 5] and D is 2 should be return '3 4 5 1 2'", () => {
     expect(leftRotation([1, 2, 3, 4, 5], 2)).toEqual("3 4 5 1 2");
+  });
+
+  test("When A is [1, 2, 3, 4, 5] and D is 3 should be return '4 5 1 2 3'", () => {
+    expect(leftRotation([1, 2, 3, 4, 5], 3)).toEqual("4 5 1 2 3");
+  });
+
+  test("When A is [1, 2, 3, 4, 5] and D is 4 should be return '5 1 2 3 4'", () => {
+    expect(leftRotation([1, 2, 3, 4, 5], 4)).toEqual("5 1 2 3 4");
+  });
+
+  test("When A is [1, 2, 3, 4, 5] and D is 5 should be return '1 2 3 4 5'", () => {
+    expect(leftRotation([1, 2, 3, 4, 5], 5)).toEqual("1 2 3 4 5");
+  });
+
+  test("When A is [1, 2, 3, 4, 5] and D is 6 should be return '2 3 4 5 1'", () => {
+    expect(leftRotation([1, 2, 3, 4, 5], 6)).toEqual("2 3 4 5 1");
+  });
+
+  test("When A is [1, 2, 3, 4, 5] and D is 781 should be return '2 3 4 5 1'", () => {
+    expect(leftRotation([1, 2, 3, 4, 5], 781)).toEqual("2 3 4 5 1");
+  });
+
+  test("When A is [1, 2, 3, 4, 5, 6, 7] and D is 781 should be return '5, 6, 7, 1, 2, 3, 4'", () => {
+    expect(leftRotation([1, 2, 3, 4, 5, 6, 7], 781)).toEqual("5 6 7 1 2 3 4");
   });
 });
--- src/leftRotation.js
+++ src/leftRotation.js
@@ -1,14 +1,26 @@
 export function leftRotation(a = [], d = 0) {
+  if (a === null) {
+    throw new Error("A value cannot be NULL");
+  }
+
+  if (a.length === 0) {
+    throw new Error("A value cannot be []");
+  }
+
+  if ([0, null, undefined].includes(d)) {
+    throw new Error("D value cannot be NULL");
+  }
+
   let rotations = 0;
 
   if (d >= a.length) {
-    rotations = a.length % d;
+    rotations = d % a.length;
   } else {
     rotations = d;
   }
 
   if (rotations === 0) {
-    return a;
+    return a.join(" ");
   }
 
   const p1 = a.slice(0, rotations);
