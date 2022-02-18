62b88c8f1527dc83f4f29cf351bcab4c699b18c5
Thiago Pereira <thiagopereiramail@gmail.com>
2022-02-18 14:53:34 +0000

    Cria o primeiro caso de teste

---
+++ __tests__/leftRotation.test.js
@@ -0,0 +1,22 @@
+/*
+Uma operação de rotação à esquerda em uma matriz desloca cada um dos elementos da matrizunidade 
+à esquerda. Por exemplo, se 2 rotações à esquerda são realizadas na matriz [1,2,3,4,5], então a 
+matriz se tornaria [3,4,5,1,2].
+
+Observe que o item de índice mais baixo se move para o índice mais alto em uma rotação. Isso é chamado de 
+matriz circular.
+
+Dada uma matriz 'A' de 'N' inteiros e um número, 'D', executar 'D' rotações para a esquerda 
+na matriz. Retorne a matriz atualizada a ser impressa como uma única linha de inteiros 
+separados por espaço.
+
+Obs.: Vou retornar apenas a matriz atualizada.
+*/
+
+import { leftRotation } from "../src/leftRotation";
+
+describe("Left Rotation", () => {
+  test("When A is [1, 2, 3, 4, 5] and D is 2 should be return [3, 4, 5, 1, 2]", () => {
+    expect(leftRotation([1, 2, 3, 4, 5], 2)).toEqual([3, 4, 5, 1, 2]);
+  });
+});
---
+++ src/leftRotation.js
@@ -0,0 +1,19 @@
+export function leftRotation(a = [], d = 0) {
+  let rotations = 0;
+
+  if (d >= a.length) {
+    rotations = a.length % d;
+  } else {
+    rotations = d;
+  }
+
+  if (rotations === 0) {
+    return a;
+  }
+
+  const p1 = a.slice(0, rotations);
+  const p2 = a.slice(rotations);
+  const result = [].concat(p2, p1);
+
+  return result;
+}
